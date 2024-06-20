915a12e697706e638e359bb7a3d6a9e0b4bfca40
pratik <pratik.phirmuhani@gmail.com>
2024-06-20 21:59:43 +0530

    third commit

--- actions/actions.py
+++ actions/actions.py
@@ -2,6 +2,8 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.forms import FormValidationAction
+from rasa_sdk.events import SlotSet
+
 
 class ActionResetPassword(Action):
     def name(self) -> Text:
